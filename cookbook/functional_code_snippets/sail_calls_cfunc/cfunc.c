9594e1f15dffbe4e4def85aeb994d87dbd848bad
William McSpaddden <bill@riscv.org>
2022-06-03 12:40:31 -0500

    fixed compile warning.  fixed seg fault

--- cookbook/functional_code_snippets/sail_calls_cfunc/cfunc.c
+++ cookbook/functional_code_snippets/sail_calls_cfunc/cfunc.c
@@ -11,25 +11,18 @@
 
 #include <sail.h>
 #include "cfunc.h"
+#include "string.h"
 
 
-//int
-//cfunc_int(void)
-//mach_bits
-//int
 INT_RET_TYPE
-//cfunc_int(unit u, int foo)
-//cfunc_int(        int foo)
-//cfunc_int(        sail_int foo)
 cfunc_int(sail_int *zret_int,  bool foo)
     {
 //    mpz_set_ui(zret_int, 142);
 //    mpz_set_ui(zret_int, 9223372036854775808 );                       // 2 ^ 64           // works
 //    mpz_set_ui(zret_int, (9223372036854775808 + 1) );                 // (2 ^ 64) + 1     // works
 //    mpz_set_ui(zret_int, (123456789012345678901234567890) );          // fails: sail.test prints out incorrect number But the next example works.
-    mpz_init_set_str(zret_int, "123456789012345678901234567890", 10 );  // works
+    mpz_init_set_str(*zret_int, "123 456 789 012 345 678 901 234 567 890", 10 );  // NOTE: white space allowed in string // works
 
-//    zret = 42;
     return(42);
     }
 
@@ -37,7 +30,30 @@ cfunc_int(sail_int *zret_int,  bool foo)
 void
 cfunc_str(sail_string * zret_str, bool foo)
     {
-    *zret_str =  "i'm baaaack...\n";
+    //=========================
+    //  The following code ......
+    //
+    //    *zret_str =  "i'm baaaack...\n";
+    //
+    //    return;
+    //
+    //  ... yields a segmentation fault when killing
+    //  the sail_string variable (pointed to by zret_str)
+    //  in the calling code.  The calling code assumes that
+    //  memory has been malloc'd for the string,  and when
+    //  it's free'd,  you get a seg fault.  So,  I re-wrote
+    //  the code to do the actual malloc. But note the 
+    //  assymetry of the memory management:  the space is
+    //  allocated here,  but free'd at the calling level.
+    //  This is,  at least,  ugly code.  And,  at worst,
+    //  prone to error.
+    //=========================
+    char *  str = "i'm baaaack....\n";
+    char *  s;
+
+    s = malloc(strlen(str));
+    strcpy(s, str);
+    *zret_str =  s;
     return;
     }
 
--- cookbook/functional_code_snippets/sail_calls_cfunc/cfunc.h
+++ cookbook/functional_code_snippets/sail_calls_cfunc/cfunc.h
@@ -19,6 +19,13 @@
 //#define INT_RET_TYPE    sail_int
 #define INT_RET_TYPE    int
 
+// It doesn't appear that Sail does anything with the
+//  function's return value.  "return values" are done
+//  by passing a pointer to a return value struct, which
+//  is the first element in the function's argument list.
+//
+//  TODO: make the return value of type void.
+
 INT_RET_TYPE    cfunc_int(sail_int *,       bool);
 void            cfunc_str(sail_string * ,    bool);
 
