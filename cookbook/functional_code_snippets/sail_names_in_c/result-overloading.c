1258d2a2e538edec4f148ba1b5c80160b00cd53f
Martin Berger <contact@martinfriedrichberger.net>
2022-07-14 12:06:28 +0100

    Added note on Sail -> C naming and overloading. (TODO: overloading of execute clauses)

---
+++ cookbook/functional_code_snippets/sail_names_in_c/result-overloading.c
@@ -0,0 +1,14 @@
+void zgiraffe1(sail_int *rop, unit);
+void zgiraffe1(sail_int *zcbz30, unit zgsz30) { ... }
+
+void zgiraffe2(sail_int *rop, sail_int);
+void zgiraffe2(sail_int *zcbz31, sail_int zn) { ... }
+
+void zmain(sail_int *zcbz32, unit zgsz32)
+{
+  ...
+  zgiraffe1(&zx1, UNIT);
+  ...
+    zgiraffe2(&zx2, zgsz33);
+  ...
+}
